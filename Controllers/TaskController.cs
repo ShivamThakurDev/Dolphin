4451d15d2b4fca6d09be238550ce151e359e9132
Shivam Thakur <shivamthakur0776@gmail.com>
2024-08-29 00:14:19 +0530

    Add project files.

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dolphin.Controllers
+{
+    public class TaskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
