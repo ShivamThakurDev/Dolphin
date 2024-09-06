6cb2d655b21f82c15b9decec6935fc978de6448e
Shivam Thakur <shivamthakur0776@gmail.com>
2024-09-07 00:06:06 +0530

    added entity mapping

---
+++ Dolphin.DAL/Model/UserDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolphin.DAL.Model
+{
+    public class UserDetails:BaseEntity
+    {
+        [Column(Order =0)]
+        public int Id { get; set; }
+        [Column(Order =1)]
+        public Guid User_Id { get; set; }
+        public virtual User User { get; set; }
+        [Column(Order =2)]
+        public Guid Role_Id { get; set; }
+        public virtual Role Role { get; set; }
+        [Column(Order =3)]
+        public Guid Team_Id { get; set; }
+        public virtual Team Team { get; set; }
+    }
+}
